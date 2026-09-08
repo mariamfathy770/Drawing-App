84155534ca41cf59229b6052994ff425b11cb754
mariamfathy770 <marrysewad7@gmail.com>
2026-09-09 02:15:29 +0300

    Add files via upload

---
+++ Drawingproject/myDrawingProject/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace myDrawingProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.btn_BackColor = new System.Windows.Forms.Button();
+            this.btn_PenColor = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(254, 222);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.trackBar1);
+            this.panel1.Controls.Add(this.btn_PenColor);
+            this.panel1.Controls.Add(this.btn_BackColor);
+            this.panel1.Controls.Add(this.btn_Clear);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(254, 52);
+            this.panel1.TabIndex = 1;
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Location = new System.Drawing.Point(151, 26);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(45, 23);
+            this.btn_Clear.TabIndex = 0;
+            this.btn_Clear.Text = "clear";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // btn_BackColor
+            // 
+            this.btn_BackColor.Location = new System.Drawing.Point(202, 28);
+            this.btn_BackColor.Name = "btn_BackColor";
+            this.btn_BackColor.Size = new System.Drawing.Size(49, 23);
+            this.btn_BackColor.TabIndex = 1;
+            this.btn_BackColor.Text = "backcolor";
+            this.btn_BackColor.UseVisualStyleBackColor = true;
+            this.btn_BackColor.Click += new System.EventHandler(this.btn_BackColor_Click);
+            // 
+            // btn_PenColor
+            // 
+            this.btn_PenColor.Location = new System.Drawing.Point(202, 3);
+            this.btn_PenColor.Name = "btn_PenColor";
+            this.btn_PenColor.Size = new System.Drawing.Size(48, 23);
+            this.btn_PenColor.TabIndex = 2;
+            this.btn_PenColor.Text = "pencolor";
+            this.btn_PenColor.UseVisualStyleBackColor = true;
+            this.btn_PenColor.Click += new System.EventHandler(this.btn_PenColor_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(3, 3);
+            this.trackBar1.Maximum = 20;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(117, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Value = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button btn_PenColor;
+        private System.Windows.Forms.Button btn_BackColor;
+        private System.Windows.Forms.Button btn_Clear;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ Drawingproject/myDrawingProject/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myDrawingProject
+{
+    public partial class Form1 : Form
+    {
+        bool isDrawing = false;
+        Graphics g;
+        Color myColor = Color.Black;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_PenColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                myColor = colorDialog1.Color;
+            }
+        }
+
+        private void btn_BackColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDrawing = true;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrawing = false;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                g = pictureBox1.CreateGraphics();
+                int thickness = trackBar1.Value;
+                Brush myBrush = new SolidBrush(myColor);
+                g.FillEllipse(myBrush, e.X, e.Y, thickness, thickness);
+            }
+        }
+    }
+}
